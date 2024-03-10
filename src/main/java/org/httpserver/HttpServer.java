7cb74cb4c2be8016c27f24cc2341b9595eec1e3c
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2024-03-10 14:59:48 +0100

    Add logic for creating response to client

--- src/main/java/org/httpserver/HttpServer.java
+++ src/main/java/org/httpserver/HttpServer.java
@@ -4,6 +4,10 @@ import org.httpserver.config.Configuration;
 import org.httpserver.config.ConfigurationManager;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Class for Http server
@@ -18,5 +22,29 @@ public class HttpServer {
 
         System.out.println("Configuration port " + config.getPort());
         System.out.println("Configuration webroot " + config.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(config.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            final String CRLF = "\n\r";
+            String html = "<html><head><title>Simple Java HTTP server</title></head><body><h1>This page was created by my simple Java HTTP server</h1></body></html>";
+            String response = "HTTP/1.1 200 OK" + CRLF + "Content-Length" + html.getBytes().length + CRLF + CRLF + html + CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            // TODO need to implement reading
+            // TODO need to implement writing
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
     }
 }
