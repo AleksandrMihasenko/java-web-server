f85b77918e7671637656c9db6ad198059e13faad
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2024-04-01 18:29:28 +0200

    Implement http parser methods

---
+++ src/main/java/org/http/HttpRequest.java
@@ -0,0 +1,18 @@
+package org.http;
+
+public class HttpRequest extends HttpMessage {
+    private String method;
+    private String requestTarget;
+    private String httpVersion;
+
+    HttpRequest() {
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    void setMethod(String method) {
+        this.method = method;
+    }
+}
